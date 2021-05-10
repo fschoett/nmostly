6825b221ef371d9078be51202952f699bc7fea6d
fschoett <fabian-schoettler@outlook.com>
2021-05-10 22:58:25 +0200

    Added some index files

--- app.ts
+++ app.ts
@@ -1,6 +1,7 @@
-import { NmosMediator } from "./src/api";
+import { Nmostly } from "./src";
 
-const nodeApi = new NmosMediator({
+
+const nodeApi = new Nmostly({
     ifaceName: "eth0",
     ipv4: "192.168.178.41",
     macAddr: "74-D4-35-BB-25-1C",
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { NmosMediator as Nmostly } from "./api/nmos-mediator";
+
+export * from "./schemas";
+export * from "./resources"
