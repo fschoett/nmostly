87657da7c61deb9c00c56cb9028e222f3c6851a2
fschoett <fabian-schoettler@outlook.com>
2021-05-10 22:53:16 +0200

    Moved app.ts to root dir

--- app.ts
+++ app.ts
@@ -1,4 +1,4 @@
-import { NmosMediator } from "./api";
+import { NmosMediator } from "./src/api";
 
 const nodeApi = new NmosMediator({
     ifaceName: "eth0",
@@ -8,19 +8,10 @@ const nodeApi = new NmosMediator({
 });
 
 nodeApi.addReceiverAudio();
-nodeApi.addReceiverAudio();
-nodeApi.addReceiverAudio();
 
 const newSourceId = nodeApi.addAudioSource();
 const flowId = nodeApi.getNode().getSource(newSourceId).getFlow().id;
 
 nodeApi.addSender( { flow_id: flowId });
 
-// nodeApi.addSender( newDeviceId, newSender );
-
-async function startup() {
-    await nodeApi.startServer();
-    console.log("App: Started server");
-}
-
-startup();
+nodeApi.startServer();
